6dae27c65e1d54f871bbc40e983f6e314a0a1a4d
gautamyogesh017 <gautamyogesh017@gmail.com>
2023-09-25 16:45:39 +0545

    minor changes

--- server/Server.js
+++ server/Server.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+
 const cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json());
 require("dotenv").config();
 
+const port = process.env.PORT;
+
 const connect = require("./src/db/mongoose");
 connect();
 
-const bookRouter = require("./src/Controller/bokRouter");
+const bokRouter = require("./src/Controller/bokRouter");
 const loginRouter = require("./src/Controller/loginRouter");
 const registerRouter = require("./src/Controller/registerRouter");
 
-app.use("/books", bookRouter);
-app.use("/register", registerRouter);
-app.use("/login", loginRouter);
+app.use("/api/books", bokRouter);
+app.use("/api/register", registerRouter);
+app.use("/api/login", loginRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Bookstore Server listening on port ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
